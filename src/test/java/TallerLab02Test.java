52f6277b88443e7e871454f3ae6b9b6d7ffd3296
prain01 <71102083+prain01@users.noreply.github.com>
2021-09-28 10:41:51 -0300

    agregando clase test

---
+++ src/test/java/TallerLab02Test.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TallerLab02Test {
+
+    @Test
+    void generarArreglo() {
+    }
+
+    @Test
+    void mayorProductoAdyacentes() {
+    }
+
+    @Test
+    void pedirNumeroLimitado() {
+    }
+
+    @Test
+    void pedirNumero() {
+    }
+
+    @Test
+    void limitesNumero() {
+    }
+}
